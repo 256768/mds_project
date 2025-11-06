23375473038f5b6bde9a4c0cb3860e1ab87513c7
Vojtěch Trunda <vojtech.trunda@vojtat.cz>
2025-11-06 01:17:55 +0100

    Player with waiting variant

--- frontend/js/player.js
+++ frontend/js/player.js
@@ -6,32 +6,36 @@ const infoCardElement = document.getElementById('infoCard');
 
 // do elementu playerElement prida funkcionalitu knihovny Video.js
 var options = {
-    fluid: true //meni se velikost dle okna
+    fluid: true, //meni se velikost dle okna
+    loop: true,
+    muted: true,
+    autoplay: true
 };
 var player = videojs(playerElement, options);
+player.play();
 
 function webcastBegin(){
-  playerContainerElement.style.display = 'block';
+  player.src({
+    type: "application/x-mpegURL",
+    src: "/hls/stream.m3u8"
+  });
+
+  player.controls(true);
+  player.muted(false);
+  broadcastInfo.style.display = 'block';
   stoppedTextElement.style.display = 'none';
-  playerCardElement.classList.remove("flex-fill");
-  playerCardElement.classList.add("col-8");
-  infoCard.style.display = 'block';
 }
 
 function webcastWait(){
+  player.src({
+    type: "video/mp4",
+    src: "assets/waiting.mp4"
+  });
 
-    player.src({
-      type: "video/mp4",
-      src: "https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"
-    });
-    player.play();
-
-  playerContainerElement.style.display = 'none';
+  player.controls(false);
+  player.muted(true);
+  broadcastInfo.style.display = 'none';
   stoppedTextElement.style.display = 'block';
-  playerCardElement.classList.remove("col-8");
-  playerCardElement.classList.add("flex-fill");
-  infoCard.style.display = 'none';
-
 }
 
 webcastWait();
