b9b54de91419c0485d3f2325f97af5ff87dd832a
Vojtěch Trunda <vojtech.trunda@vojtat.cz>
2025-11-16 00:28:58 +0100

    Stream on/off switch, error dialog, WebSocket client (untested)

--- frontend/js/broadcaster.js
+++ frontend/js/broadcaster.js
@@ -1,10 +1,19 @@
-const playerElement = document.getElementById('hlsPlayer');
+const playerElement = document.getElementById('webcamPreview');
 const loginCardElement = document.getElementById('loginCard');
 const broadcastGroupElement = document.getElementById('broadcastGroup');
 const broadcasterNameElement = document.getElementById('broadcasterName');
 const microphoneStateElement = document.getElementById('microphoneState');
 const unmuteButtonElement = document.getElementById('unmuteButton');
 const muteButtonElement = document.getElementById('muteButton');
+const streamStateElement = document.getElementById('streamState');
+const startButtonElement = document.getElementById('startButton');
+const stopButtonElement = document.getElementById('stopButton');
+const controlsElement = document.getElementById('controls');
+const errorDialogElement = document.getElementById('errorDialog');
+const errorNameElement = document.getElementById('errorName');
+
+var ws;
+var mediaRecorder;
 
 // do elementu playerElement prida funkcionalitu knihovny Video.js
 var options = {
@@ -19,6 +28,7 @@ player.play();
 function login(){
     loginCardElement.style.display = 'none';
     broadcastGroupElement.style.removeProperty('display');
+    getWebcamAndMicrophone();
 }
 
 function logout(){
@@ -27,49 +37,100 @@ function logout(){
 }
 
 function setName(){
-    
+    //restart stream with different name+
+    stopStream();
+    startStream();
 }
 
 function muteMicrophone(){
     microphoneStateElement.innerHTML = "Mikrofon je ztlumený"
     unmuteButtonElement.style.removeProperty('display');
     muteButtonElement.style.display = 'none';
+
+    if(typeof window.mediaRecorder !== 'undefined'){
+        window.mediaRecorder.stream.getAudioTracks().forEach(function(track){track.enabled = false;});
+    }
 }
 
 function unmuteMicrophone(){
     microphoneStateElement.innerHTML = "Mikrofon je zapnutý"
     unmuteButtonElement.style.display = 'none';
     muteButtonElement.style.removeProperty('display');
-}
 
-logout();
-muteMicrophone();
-
-function getWebcamAndMicrophone(el,config, onPlaying){
-    //check for webcams (or other video devices)
-    const hasGetUserMedia = () => !!navigator.mediaDevices?.getUserMedia;
-
-    if(hasGetUserMedia()){
-        // only run's once, and passes the stream, to the video element
-        navigator.mediaDevices.getUserMedia(config).then(function(stream) {
-            if(!el)
-                return
-            el.srcObject = stream;
-            console.log(stream);
-            el.play();
-            onPlaying()
-        })
-        .catch(function(err) {
-            console.log("Nastala chyba " + err);//(this error gets reached instead)
-        });
-    }else{
-        console.log("Webkamera nenalezena");//this error cannot be reached
+    if(typeof window.mediaRecorder !== 'undefined'){
+        window.mediaRecorder.stream.getAudioTracks().forEach(function(track){track.enabled = false;});
     }
 }
 
-function testWebcam(){
-    getWebcamAndMicrophone(player.tech().el(), { video: true, audio: true }, () => {
-        streaming = true;
-        console.log("Webkamera je v náhledu")
+function startStream(){
+    window.ws = new WebSocket('ws://localhost:3000/broadcast?name=' + broadcasterNameElement.value);
+    window.ws.binaryType = 'arraybuffer';
+    window.ws.onopen = () => {
+        window.mediaRecorder = new MediaRecorder(playerElement.captureStream(30), { mimeType: 'video/webm; codecs=vp8' });
+        window.mediaRecorder.ondataavailable = e => {
+        if (e.data.size > 0 && window.ws.readyState === WebSocket.OPEN) {
+            window.ws.send(e.data);
+        }
+        };
+        window.mediaRecorder.start(200); //send 200ms chunks
+
+        //ui gets updated only if connection successful
+        muteButtonElement.disabled = false;
+        streamStateElement.innerHTML = "Vysílání je spuštěné"
+        startButtonElement.style.display = 'none';
+        stopButtonElement.style.removeProperty('display');
+    };
+    window.ws.onerror = function (error) {
+        alert("Vysílání se nepodařilo zahájit");
+        console.log(error);
+    };
+}
+
+function stopStream(){
+    streamStateElement.innerHTML = "Vysílání je zastavené"
+    startButtonElement.style.removeProperty('display');
+    stopButtonElement.style.display = 'none';
+
+    if(typeof window.mediaRecorder !== 'undefined'){
+        window.mediaRecorder.stop();
+    }
+    if(typeof window.ws !== 'undefined'){
+        window.ws.close();
+    }
+}
+
+function getWebcamAndMicrophone(){
+    // set webcam to preview
+    navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(function(stream) {
+        player.tech().el().srcObject = stream;
+        console.log(stream);
+        player.tech().el().play();
+        console.log("Webkamera je v náhledu");
+    })
+    .catch(function(err) {
+        console.log("Nastala chyba " + err);
+        showError(err);
     });
 }
+
+function showError(e){
+    switch(e.name){
+        case "NotFoundError":
+            var error = "webkamera a mikrofon nebyly nalezeny";
+            break;
+        case "NotAllowedError":
+            var error = "nebyl umožněn přístup ke kameře a mikrofonu";
+            break;
+        default:
+            var error = e;
+            break;
+    }
+    alert("Nastala chyba: "+error);
+    errorNameElement.innerHTML = error;
+    errorDialogElement.style.removeProperty('display');
+    controlsElement.style.display = 'none';
+}
+
+logout();
+unmuteMicrophone();
+stopStream();
