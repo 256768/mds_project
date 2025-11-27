3fd05e699341999697d2f97a8d095a57c1711351
256768 <256768@vutbr.cz>
2025-11-27 11:04:19 +0100

    Refactor server.js: Express handles only /api/auth, HLS served by Nginx

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-import express from "express";
 import http from "http";
 import dotenv from "dotenv";
 import authRoutes from "./auth/routes.js";
 import { startWebsocketServer } from "./websocket.js";
+import express from "express";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -16,19 +16,19 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// API route
+// api route pro auth
 app.use("/api/auth", authRoutes);
 
-// HLS vystup
-const hlsOutputDir = path.join(__dirname, "hls", "output");
-console.log("Serving HLS from:", hlsOutputDir);
-app.use("/hls", express.static(hlsOutputDir));
-
-// HTTP server
+// http server pro api a websocket
 const server = http.createServer(app);
 
-// WebSocket server pro broadcaster
+// cesta k hls pro websocket (hls serviruje nginx)
+const hlsOutputDir = path.join(__dirname, "hls", "output");
+console.log("HLS output directory (for WebSocket):", hlsOutputDir);
+
+// websocket server
 startWebsocketServer(server, hlsOutputDir);
 
+// spuštění serveru
 const PORT = 3001;
 server.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
