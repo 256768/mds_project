9735a0bf6f4d753f3f8ffc6020fc3329d55b5d2e
Vojtěch Trunda <vojtech.trunda@vojtat.cz>
2025-12-13 16:44:33 +0100

    Removed unused files

--- stream2hls.bat
+++
@@ -1,6 +0,0 @@
-REM Script for Windows because exec refuses to work
-ffmpeg -y -i rtmp://localhost/broadcast/stream -map 0:v -map 0:a ^
-              -c:v libx264 -c:a aac -b:v 256k -b:a 32k -vf scale='640:360' -preset veryfast -f flv rtmp://localhost/hls/stream_360 ^
-              -c:v libx264 -c:a aac -b:v 384k -b:a 64k -vf scale='854:480' -preset veryfast -f flv rtmp://localhost/hls/stream_480 ^
-              -c:v libx264 -c:a aac -b:v 1920k -b:a 128k -vf scale='1280:720' -preset veryfast -f flv rtmp://localhost/hls/stream_720 ^
-			  -c copy -f flv rtmp://localhost/hls/stream_src
--- utils/streamKey.js
+++
@@ -1,5 +0,0 @@
-import crypto from "crypto";
-
-export function generateStreamKey(userId) {
-  return `${userId}-${crypto.randomBytes(12).toString("hex")}`;
-}
